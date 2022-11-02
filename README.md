dc566a52fbee94ad17a3736b9690de9d2e5f35e6
sharvari-p <spradhan6152@sdsu.edu>
2022-11-02 13:32:34 -0700

    CS648.01-Assignment-9

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# CS-648.01-Assignment-9
+### Introduction
+In this Assignment we covered the JQuery features and concepts such as Event handling, commands, selector & form manupulations. We also covered manipulation of DOM by using selector methods and manupulation of events. 
+
---
+++ scripts/event-tuts.js
@@ -0,0 +1,107 @@
+$(function() {
+    // 1
+    $("button")
+        .click(function() {
+            alert("The button has been clicked!");
+        });
+    
+    // 2
+    $("button")
+        .bind("click", function() {
+            alert("The button has been clicked!");
+        });
+                    
+    // 3
+    $("body")
+        .on("click", function() {
+            alert("The button has been clicked!");
+        });
+    
+    // 4
+    $("body")
+        .on("click", "button", function() {
+            alert("The button has been clicked!");
+        });
+    
+    // 5
+    $("div")
+        .css("width", "400px")
+        .css("height", "400px")
+        .css("background", "silver");
+    
+    $("body")
+        .on("click mouseenter mouseleave", "div", function() {
+        alert("The div was either clicked, entered, or left!");
+    });
+    
+    // 6
+    $("span")
+        .css("display", "block")
+        .css("width", "200px")
+        .css("height", "200px")
+        .css("background", "silver");
+    
+    $("body").on({
+        click: function() {
+            alert("The span was clicked!");
+        },
+        mouseenter: function() {
+            alert("The span was moused into!");
+        },
+        mouseleave: function() {
+            alert("You left the span tag!");
+        }
+    }, "span");
+    
+    
+    // 7
+    
+    $("body")
+        .on("click", "a", function(event) {
+        event.preventDefault();
+    
+        $("a")
+        .css("color", "red");
+        alert(event.target.nodeName);
+    });
+    
+    
+    // 8
+    $(window)
+        .resize(function() {
+        $("div")
+            .text($(window).width());
+    });
+
+    // 9
+    
+    $("input")
+        .on({
+            focus: function() {
+                $(this).css("background", "#EDEDED");
+            },
+            blur: function() {
+                $(this).css("background", "#FFFFFF");
+            }
+    });
+    
+    
+    // 10
+    
+    $("body").on({
+        submit: function(event) {
+            event.preventDefault();
+            if ($("#name").val().length > 0) {
+                $("#name").css("border", "1px solid green");
+            } else {
+                $("#name").css("border", "1px solid red");
+            }
+            if ($("#email").val().length > 0) {
+                $("#email").css("border", "1px solid green");
+            } else {
+                $("#email").css("border", "1px solid red");
+            }
+        }
+    }, "form");
+    
+});
